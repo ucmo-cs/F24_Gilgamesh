60d85782df8138d8d68cb4d283c295c530a416ff
noahBowling <nab08580@ucmo.edu>
2024-10-30 12:15:23 -0500

    deleted a back up
    deleted a back up

--- Backend/CommercBank - Copy/CommercBank/src/main/java/com/commercBank/CommercBank/Repository/LoanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.commercBank.CommercBank.Repository;
-
-import com.commercBank.CommercBank.Domain.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepository extends JpaRepository<Loan, Long> {
-
-}
-

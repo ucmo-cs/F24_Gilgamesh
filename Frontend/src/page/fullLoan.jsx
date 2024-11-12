158f291538d5695a691f2ccc278db38d070a467c
Connor Robinet <101066798+PHDSampson@users.noreply.github.com>
2024-11-11 22:14:43 -0600

    V

--- Frontend/src/page/fullLoan.jsx
+++ Frontend/src/page/fullLoan.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+import './fullLoan.css';
+
 function fullLoan() {
-    
+    return (
+        <div className="background">
+            {/* Content goes here */}
+            <h1>Welcome to the Full Loan Page!</h1>
+        </div>
+    );
 }
 
 export default fullLoan;

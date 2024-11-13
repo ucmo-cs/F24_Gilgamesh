3d45c1aeba9a4b91e5de085a92b2ec3c913850c5
Jkutney21 <108850690+Jkutney21@users.noreply.github.com>
2024-11-13 12:28:35 -0600

    a
    I need to

--- Frontend/src/page/Admin.jsx
+++ Frontend/src/page/Admin.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import Spreadsheet from '../components/Spreadsheet';
+import { Modal,Button } from 'react-bootstrap';
+import UserLoanForm from '../components/AdminForm';
 import './Admin.css'; // Import your CSS file here
+import AdminForm from '../components/AdminForm';
+
+
+// Handle opening and closing of Loan Payment modal
+const handleClose1 = () => setShow1(false);
+const handleShow1 = () => setShow1(true);
 
 const Admin = () => {
   return (
     <div className="admin-container">
       <div className="box">
         <Spreadsheet />
+        <div>
+        <Button variant="secondary" onClick={handleShow1} className="ms-2">
+              Pay Loan
+          </Button>
+          {/* Modal for Loan Payment Form */}
+          <Modal show={show1} onHide={handleClose1} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+           <Modal.Header closeButton>
+          <Modal.Title>Loan Payment Form</Modal.Title>
+         </Modal.Header>
+        <Modal.Body className="d-flex flex-column" style={{ height: '60vh' }}>
+          <UserLoanForm loanValue={AdminForm}  />
+        </Modal.Body>
+      </Modal>
+          
+         </div>  
       </div>
       
     </div>
   );
 }
+
+
+
+
+      
+    
 
 export default Admin
